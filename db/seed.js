c4a9f7b1c6501e9a1ff23d5226dbdae7110fbf64
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-08-14 15:08:11 -0500

    final commit of this branch

--- db/seed.js
+++ db/seed.js
@@ -3,3 +3,10 @@ const mongoose = require('./connection');
 const seeds = require('./seeds.json');
 
 const Game = require('../models/Game');
+
+Game.deleteMany({}).then(() => {
+	Game.insertMany(seeds).then(() => {
+		console.log('done yo!');
+		process.exit();
+	});
+});
