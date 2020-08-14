09dc76fe5ff24fae28420b3875a68757e96f9734
chollingsworth <caleb.hollingsworth10@gmail.com>
2020-08-14 15:02:25 -0500

    added seed data and fleshed out api before routers are in place with Mike

---
+++ db/seed.js
@@ -0,0 +1,5 @@
+const mongoose = require('./connection');
+
+const seeds = require('./seeds.json');
+
+const Game = require('../models/Game');
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const gamesController = require('./controllers/games');
 const cors = require('cors');
 
 const app = express();
@@ -6,5 +7,10 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+	res.redirect('/api/gamez');
+});
+app.use('/api/gamez', gamesController);
 
 app.listen(3000, () => console.log('pimpin on 3000'));
